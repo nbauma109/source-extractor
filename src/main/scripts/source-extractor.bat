8526880e764ec543271f3b5f4c1554b7c9591abd
nbauma109 <nbauma109@github.com>
2024-04-06 20:53:32 +0200

    script source-extractor: enforce order of execution

---
+++ src/main/scripts/source-extractor.bat
@@ -0,0 +1,3 @@
+@echo off
+call .\bin\java-source-extractor.bat %*
+call .\bin\scala-source-extractor.bat %*
---
+++ src/main/scripts/source-extractor.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+./bin/java-source-extractor "$@"
+./bin/scala-source-extractor "$@"
